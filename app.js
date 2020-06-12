a7b9c1066ddffba307db82ad192a698b28b8d23d
Alexey Sidorov <2176545@gmail.com>
2020-06-12 18:02:52 +0300

    crash-test added

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ const startDatabase = async () => {
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
