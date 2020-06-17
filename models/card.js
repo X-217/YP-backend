33ecce24b4aa4448cee4b464827b1491f95736c2
Alexey Sidorov <2176545@gmail.com>
2020-06-18 01:39:40 +0300

    bags fixed

--- models/card.js
+++ models/card.js
@@ -7,7 +7,7 @@ const cardSchema = new mongoose.Schema({
     required: [true, 'Card name required'],
     minlength: 2,
     maxlength: 30,
-    match: /^\S.*\S$/gm,
+    match: /^\S.*\S$/,
   },
   link: {
     type: String,
